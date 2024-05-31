bdb9e4eb5f26e3b891c654bd128adfa7c3cce2ad
Senann945 <senanrasulov2@gmail.com>
2024-05-31 14:07:28 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,158 @@
+
+// 1.Arrayin uzunluğun tapmaq
+
+
+// const arr = [1, 2, 3, 4, 5,6,7,8,9];
+
+// const arrayLength = arr.length;
+// console.log(arr.length);
+
+
+
+// 2.At funksiyası ilə sonuncu elementin adını tapmaq
+
+
+
+
+// const Arr = [1, 2, 3, 4, 5,6,7,8];
+
+// const sonuncuElement = Arr.at(-1);
+
+// console.log(sonuncuElement);  
+
+
+
+
+// const nameArray = ["Senan", "Kamil", "Polad", "Ferid","Murad"];
+
+// const sonuncuElement = nameArray[nameArray.length - 1];
+
+// console.log(sonuncuElement);
+
+
+// const employees = ["Senan", "Ferid", "Murad", "Kamil"];
+
+
+// employees.push("Polad");
+
+// console.log(employees);
+
+
+
+// const employees = ["Senan", "Ferid", "Murad", "Kamil"];
+
+
+// employees.unshift("Polad");
+
+// console.log(employees);
+
+
+
+// const employees = ["Senan", "Ferid", "Murad", "Kamil"];
+
+//  employees.pop();
+
+//  console.log(employees);
+
+
+
+// const employees = ["Senan", "Ferid", "Murad", "Kamil"];
+
+
+// employees.shift();
+
+// console.log(employees);
+
+
+//  const employees = ["Senan", "Ferid", "Murad", "Kamil"];
+
+
+//  const newEmployees = employees.join( "-");
+
+//  console.log(newEmployees);
+
+
+
+
+// const ters = [1, 2, 3, 4, 5];
+
+
+// ters.reverse();
+
+
+// console.log(ters);
+
+
+
+//  const employees = [
+//     { name: "Kamil", position: "Backend" },
+//     { name: "Senan", position: "Frontend" },
+//     { name: "Ferid", position: "Fullstack" },
+//     { name: "Polad", position: "Frontend" }
+// ];
+
+// const frontendss = employees.some(employee => employee.position === "Frontend");
+
+// console.log(frontendss);
+
+// !  include yoxla
+
+
+
+// const employees = [
+//     { name: "Senan", age: 23 },
+//     { name: "Kamil", age: 21 },
+//     { name: "Murad", age: 23 },
+//     { name: "Polad", age: 18 }
+// ];
+
+// const allAge = employees.every(employee => employee.age > 18);
+
+// console.log(allAge);
+
+// const employees = [
+//     { name: "Kamil", position: "Backend" },
+//     { name: "Murad", position: "Fullstack" },
+//     { name: "Senan", position: "Frontend | Team Leader" },
+//     { name: "Ferid", position: "Backend" }
+// ];
+
+
+// const index = employees.findIndex(employee => employee.position === "Frontend | Team Leader");
+
+// console.log(index);
+
+
+
+
+// const employees = [
+//     { name: "Kamil", position: "Backend" },
+//     { name: "Murad", position: "Fullstack" },
+//     { name: "Senan", position: "Frontend | Team Leader" },
+//     { name: "Ferid", position: "Backend" }
+// ];
+
+
+// const find = employees.find(employee => employee.position === "Frontend | Team Leader");
+
+// console.log(find); 
+
+
+// const employees = [
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 22 },
+//     { name: "Charlie", age: 19 },
+//     { name: "David", age: 30 }
+// ];
+
+
+// const employees18 = employees.filter(employee => employee.age > 18);
+
+// console.log(employees18);
+
+// !sonuncunu tamamla..
+
+
+
+
+
